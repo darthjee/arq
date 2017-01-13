e35e88f7e5e373e9c608d12b9d3e5a5ed8732617
darthjee <darthjee@gmail.com>
2017-01-13 17:34:47 -0200

    Stop key override

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 (function(module, env) {
-  env.AWS_ACCESS_KEY_ID = env.ACCESS_KEY_ID;
-  env.AWS_SECRET_ACCESS_KEY = env.SECRET_ACCESS_KEY;
-
   var region = env.REGION,
       instanceId = process.env.INSTANCE_ID,
       Machine = require('./lib/machine');
